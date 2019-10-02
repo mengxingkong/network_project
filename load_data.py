110fc9b90f0c56799ef68db67b1d344110f6cad6
baiyang <993945204@qq.com>
2019-10-02 17:05:06 +0800

    load data

---
+++ load_data.py
@@ -0,0 +1,24 @@
+from pandas import DataFrame
+
+def load_data():
+    '''
+    从原始文件中读入数据并且保存
+    :return:
+    '''
+    file_path = " " # 输入文件所在文件夹
+    file_name = file_path + "web-edu.mtx"
+
+    df = DataFrame(0, columns=range(1, 3032), index=range(1, 3032))\
+
+    with open(file_name) as f:
+        lines = f.readlines()
+        lines = lines[2:]
+        for line in lines:
+            line = line.strip().split(' ')
+            df.loc[int(line[0]), int(line[1])] = 1; df.loc[int(line[1]), int(line[0])] = 1
+    df.to_csv("data.csv")
+
+if __name__ == "__main__":
+    load_data()
+
+
