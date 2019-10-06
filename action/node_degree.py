2cb37bdc1112d590db90527e87aa73ffd38e16ef
mxkstar <zlijun521@gmail.com>
2019-10-06 22:06:10 +0800

    适配 前台

--- action/node_degree.py
+++ action/node_degree.py
@@ -6,6 +6,7 @@ sys.path.append(rootPath)
 import numpy as np
 from analysis.data_reader import DataReader
 import matplotlib.pyplot as plt
+import json
 
 # 计算每个节点的度 直接将矩阵的一行或者求和 就可以得到对应节点的度数
 def node_degree(net_array=[]):
@@ -34,6 +35,30 @@ def degree_distribution(degree_array=[]):
         print("请传入节点的度数矩阵")
         return
 
+# 因为在前端显示需要进行 节点 json数据的保存
+def write_node_degree_to_file(degree_array=[], file_path="../data/net_node_degree.json"):
+    node_list = list()
+    for i in range(1, degree_array.size):
+        node_dict = {"name":i,"value":degree_array[i],"symbolSize":degree_array[i]}
+        node_list.append(node_dict)
+    all_node_degree_json = json.dumps(node_list)
+    with open(file_path, 'w') as f:
+        f.write(all_node_degree_json)
+
+# 显示网络关系图谱 需要 节点之间的连接
+def write_node_link_to_file(original_file_path="../data/web-edu.mtx", target_file_path="../data/all_node_link.json"):
+    link_list = list()
+    with open(original_file_path) as f:
+        lines = f.readlines()[2:]
+        for line in lines:
+            head, tail = [int(x) for x in line.split()]
+            link_dict = {"source":tail,"target":head}
+            link_list.append(link_dict)
+    all_node_link_json = json.dumps(link_list)
+    with open(target_file_path, 'w') as f:
+        f.write(all_node_link_json)
+
+    
 
 if __name__ == "__main__":
     reader = DataReader()
@@ -45,16 +70,23 @@ if __name__ == "__main__":
     # 因为我们的数组里面包含了一个 0 节点 （在实际网络中是不存在的） 所以在计算过程这种要排除
     print(np.min(degree_array[1:])) # 1
     print(degree_array.size)
-    distri_array = degree_distribution(degree_array)
-    print(distri_array)
 
-    # 计算度概率之和 
-    print(np.sum(distri_array)) # 1.0000000000000002  概率和为1
-    print(distri_array.size)
 
-    print( np.where( distri_array == np.max(distri_array) ) ) # 3 度为三的几点数目是最多的
+    # 生成 json 数据、
+    # write_node_degree_to_file(degree_array)
+    write_node_link_to_file()
+    # # 计算节点度分布
+    # distri_array = degree_distribution(degree_array)
+    # print(distri_array)
 
-    plt.plot(range(0,distri_array.size), distri_array, 'o-')
-    plt.show()
+    # # 计算度概率之和 
+    # print(np.sum(distri_array)) # 1.0000000000000002  概率和为1
+    # print(distri_array.size)
+
+    # print( np.where( distri_array == np.max(distri_array) ) ) # 3 度为三的几点数目是最多的
+    
+    # #绘制度分布图
+    # plt.plot(range(0,distri_array.size), distri_array, 'o-')
+    # plt.show()
 
 
