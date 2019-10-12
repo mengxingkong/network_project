8c8a94efafb21a12e68384c519a4347cbc5b04be
mxkstar <zlijun521@gmail.com>
2019-10-12 22:49:56 +0800

    submap

--- action/unicom_submap.py
+++ action/unicom_submap.py
@@ -5,6 +5,9 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 import numpy as np
 from analysis.data_reader import DataReader
+from delete_nodes import delete_nodes
+import json
+
 # 计算删除节点之后的联通子图的个数
 def calculate_unicom_submap(net_array=[]):
     if net_array != []:
@@ -34,7 +37,24 @@ def bfs_submap(net_array=[], source=[]):
                         next_level.append(i)
     return seen
 
+def write_submap_to_file(net_array, file_path="../data/submap/submap_counts.json"):
+    x_list = [0, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    y_dict = dict()
+
+    for t in range(10):
+        y_list = []
+        for i in x_list:
+            # 删除i比例的节点
+            nodes_deleted = delete_nodes(net_array, i)
+            y_list.append( calculate_unicom_submap(nodes_deleted) )
+        y_dict.update({str(t):y_list})
+
+    submap_dict = {"x":x_list,"y":y_dict}
+    with open(file_path, 'w') as f:
+        f.write( json.dumps(submap_dict) )
+
 if __name__ == "__main__":
     reader = DataReader()
     net_array = reader.data_reader()
-    print(calculate_unicom_submap(net_array))
+
+    write_submap_to_file(net_array)
